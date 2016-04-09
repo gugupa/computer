806146c39c3283cc3cecd3de9800b522f3fe1151
gugupa <yf.du@163.com>
2016-04-09 22:40:26 +0800

    删了文件夹里的文件

--- computer/src/action/CommentAction.java
+++
@@ -1,9 +0,0 @@
-package action;
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
-
-@Controller(value="CommentAction")  @Scope("prototype")
-public class CommentAction {
-
-}
